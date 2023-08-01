585a68a85ab6241004362536823876911090f160
Walter Buczacka <walterbuczacka@Walters-MacBook-Pro.local>
2023-08-01 13:08:52 -0300

    mdl

--- middleware.ts
+++ middleware.ts
@@ -1,44 +1,16 @@
 import {NextResponse} from 'next/server'
 import type {NextRequest} from 'next/server'
-import {jwtVerify} from 'jose'
-
-const verifyJWT = async (jwt: string) => {
-  const {payload} = await jwtVerify(
-    jwt,
-    new TextEncoder().encode('somesecretevaluejwt'),
-  )
-
-  return payload
-}
 
 export function middleware(request: NextRequest) {
   const jwt = request.cookies.get('__cookie_custom_name')
 
   if (!jwt) {
     return NextResponse.redirect(new URL('/signin', request.url))
+  } else {
+    return NextResponse.redirect(new URL('/home', request.url))
   }
-
-  verifyJWT(jwt.value).then(
-    () => {
-      if (request.nextUrl.pathname === '/') {
-        return NextResponse.redirect(new URL('/home', request.url))
-      }
-      const requestHeaders = new Headers(request.headers)
-      const response = NextResponse.next({
-        request: {
-          // New request headers
-          headers: requestHeaders,
-        },
-      })
-
-      return response
-    },
-    err => {
-      return NextResponse.redirect('/error')
-    },
-  )
 }
 
 export const config = {
-  matcher: ['/', '/home'],
+  matcher: '/',
 }
